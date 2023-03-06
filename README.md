dbec25ac985744bad302d629ea393e0f9906e213
chaolei.wang <link.wang@gtechdigi.com>
2023-03-06 09:53:56 +0800

    添加beanWrapper使用，xxxAware理解

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # spring_study
 用于学习spring相关知识
+
+#### 组件理解:
+BeanWrapper :  <br />
+   spring 提供的操作javaBean属性的工具类
+````
+BeanWrapper bw = PropertyAccessorFactory.forBeanPropertyAccess(new Object());
+bw.setPropertyValue("设置属性key","设置属性value");
+````
+xxxAware : <br />
+   spring 提供，如果某个类想获取xxx，则通过实习xxxAware来获取
--- src/main/java/com/study/spring_study/controller/DemoController.java
+++ src/main/java/com/study/spring_study/controller/DemoController.java
@@ -1,14 +1,23 @@
 package com.study.spring_study.controller;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+public class DemoController implements EnvironmentAware {
 
     @GetMapping("ok")
-    public String ok(){
+    public String ok() {
         return "ok";
+    }
+
+    private Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 
 
